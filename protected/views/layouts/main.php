ce9c6f90b74eecb75a5e3b9362a62bb7c7dc4f5c
napache <napache02@gmail.com>
2014-06-19 15:42:40 -0300

    Agrego cambios al menu de presentacion forma de entrar http://localhost/yii/sistema/. Quedan mas cambios pero voy de apoco con el bootsrap porque veo hay cosas que no me deja cambiar.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,42 +23,64 @@
 
     ?>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	
+	<link rel= "stylesheet" href="css/bootstrap.css">
 </head>
 
 <body>
+<div class="page-header">
+		<h1>Bienveniedos a Inmobiliaria S.A</h1>
+		<div class= "container-fluid">
+			<p> <img class ="img-rounded" src="casa.jpg"></img></p>
+		</div>
+		<div>
+			<ul class="nav nav-pills">
 
-<div class="container-fluid">
-<ul class="nav nav-pills">
- <li> <a href="/yii/sistema/" class="btn btn-primary btn-large"><i class="icon-white icon-home"></i> Inicio</a> </li>
- <li> <a href="#" class="btn btn-primary btn-large"><i class="icon-white icon-calendar"></i> Calendario</a> </li>
- <li> <a href="/yii/sistema/site/contact" class="btn btn-primary btn-large"><i class="icon-white icon-envelope"></i> Contacto</a> </li>
-     <form class="navbar-form navbar-left" role="search">
-        <input type="text" class="search-query" placeholder="Buscar" />        
-        <label for="mySubmit" class="btn"><i class="icon-search icon-white"></i> Buscar</label>
-        <input id="mySubmit" type="submit" value="Go" class="hidden" />
-     </form>    
-</ul>
+
+			 <li> <a href="/yii/sistema/" class="btn btn-primary btn-large"><i class="icon-white icon-home"></i> Inicio</a> </li>
+			 <li> <a href="#" class="btn btn-primary btn-large"><i class="icon-white "></i> Mision</a> </li>
+			 <li> <a href="#" class="btn btn-primary btn-large"><i class="icon-white"></i> Vision</a> </li>
+			 <li> <a href="/yii/sistema/alquileres" class="btn btn-success btn-large"><i class="icon-white "></i> Alquileres</a> </li>
+			 <li> <a href="/yii/sistema/ventas" class="btn btn-success btn-large"><i class="icon-white "></i> Ventas</a> </li>
+			 <li> <a href="#" class="btn btn-primary btn-large"><i class="icon-white icon-calendar"></i> Calendario</a> </li>
+			 <li> <a href="/yii/sistema/site/contact" class="btn btn-primary btn-large"><i class="icon-white icon-envelope"></i> Contacto</a> </li>    
+			
+			     <form class="navbar-form navbar-left" role="search">
+
+			        <input type="text" class="search-query" placeholder="Buscar" />        
+			        <label for="mySubmit" class="btn btn-primary btn-small"><i class="icon-search icon-white"></i> Buscar</label>
+
+			        <input id="mySubmit" type="submit" value="Go" class="hidden" />
+			     </form>    
+
+			</ul>
+
+		</div>
 </div>
-
 <div id="container">
+	<ul class="nav pull-left">
+	<li> <a href="/yii/sistema/site/casas" class="btn btn-success btn-mini"><i class="icon-white"></i> Casas</a> </li>
+	<li> <a href="/yii/sistema/site/apartamentos" class="btn btn btn-success btn-mini"><i class="icon-white "></i> Apartamentos</a> </li>
+	<li> <a href="/yii/sistema/site/campos" class="btn btn-success btn-mini"><i class="icon-white"></i> Campos</a> </li>
+	    
+	</ul>
 
+	<div id="main">
+	<?php if(isset($this->breadcrumbs)):?>
+			<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+				'links'=>$this->breadcrumbs,
+			)); ?><!-- breadcrumbs -->
+		<?php endif?>
 
-<div id="main">
-<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-</div>
+		<?php echo $content; ?>
+	</div>
 </div>
 
  <div id="footer">
 
-     	Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+     	Copyright &copy; <?php echo date('Y'); ?> Inmobiliaria PHP.<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		
  </div><!-- footer -->
 
 </div><!-- page -->
---
+++ protected/views/site/campos.php
@@ -0,0 +1,20 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+
+$this->pageTitle=Yii::app()->name . ' Campos';
+$this->breadcrumbs=array(
+	'Campos',
+);
+?>
+
+<h1>Campo</h1>
+
+
+<p>
+Esta es la pagina para mostrar los campos
+</p>
+
+
+<?php endif; ?>
