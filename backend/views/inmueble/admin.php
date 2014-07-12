21487be10e249ab5365407f890d03de985d0e5bb
vanecas <caseres.vanessa@gmail.com>
2014-07-12 16:02:04 -0300

    Se arregla lo que se muestra en Clientes

--- backend/views/inmueble/admin.php
+++ backend/views/inmueble/admin.php
@@ -24,20 +24,18 @@ $('.search-form form').submit(function(){
 	return false;
 });
 ");
+
 ?>
 
 <h1>Gestionar Inmuebles</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 )); ?>
+
 </div><!-- search-form -->
 
 
@@ -65,8 +63,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'y',
 		*/
 		array(
-			'class'=>'CButtonColumn',			
-        	
+			'class'=>'CButtonColumn',			        	
 		),
 	),
 )); ?>
