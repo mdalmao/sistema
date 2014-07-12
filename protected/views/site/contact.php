7eaa7d072a339eaee991e6c8af3d4894c718ef3e
klanata <klanata@hotmail.com>
2014-07-12 02:14:58 -0300

    contacto front bien

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -38,56 +38,56 @@ Si desea comunicarse con nuestra empresa, ingrese sus datos en el formulario a c
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-<<<<<<< Updated upstream
+
 		<!-- <?php echo $form->labelEx($model,'name'); ?> -->
-	 	<?php echo "Nombre" ?>
-=======
+	 	
+
 		<?php echo $form->labelEx($model,'Nombre'); ?>
->>>>>>> Stashed changes
+
 		<?php echo $form->textField($model,'name'); ?>
 		<?php echo $form->error($model,'name'); ?>
 	</div>
 
 	<div class="row">
-<<<<<<< Updated upstream
+
 		<!-- <?php echo $form->labelEx($model,'email'); ?> -->
-		<?php echo "Email *";?>
-=======
+		
+
 		<?php echo $form->labelEx($model,'Email'); ?>
->>>>>>> Stashed changes
+
 		<?php echo $form->textField($model,'email'); ?>
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
 	<div class="row">
-<<<<<<< Updated upstream
+
 		<!-- <?php echo $form->labelEx($model,'telefono'); ?> -->
-		<?php echo "Telefono"; ?>
-=======
+		
+
 		<?php echo $form->labelEx($model,'Telefono'); ?>
->>>>>>> Stashed changes
+
 		<?php echo $form->textField($model,'telefono'); ?>
 		<?php echo $form->error($model,'telefono'); ?>
 	</div>
 
 	<div class="row">
-<<<<<<< Updated upstream
+
 		<!-- <?php echo $form->labelEx($model,'subject'); ?> -->
-		<?php echo "Asunto"; ?>
-=======
+	
+
 		<?php echo $form->labelEx($model,'Asunto'); ?>
->>>>>>> Stashed changes
+
 		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'subject'); ?>
 	</div>
 
 	<div class="row">
-<<<<<<< Updated upstream
+
 		<!-- <?php echo $form->labelEx($model,'body'); ?> -->
-		<?php echo "Mensaje"; ?>
-=======
+		
+
 		<?php echo $form->labelEx($model,'Mensaje'); ?>
->>>>>>> Stashed changes
+
 		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'body'); ?>
 	</div>
@@ -100,13 +100,13 @@ Si desea comunicarse con nuestra empresa, ingrese sus datos en el formulario a c
 		<?php $this->widget('CCaptcha'); ?>
 		<?php echo $form->textField($model,'verifyCode'); ?>
 		</div>
-<<<<<<< Updated upstream
+
 		<div class="hint">Por favor introduce las letras tal como se muestran en la imagen de arriba. 
 Las letras no distinguen entre mayúsculas y minúsculas.</div>
-=======
+
 		<div class="hint">Por favor ingresa las letras que aparecen en el captcha.
 		<br/>No es case-sensitive.</div>
->>>>>>> Stashed changes
+
 		<?php echo $form->error($model,'verifyCode'); ?>
 	</div>
 	<?php endif; ?>
